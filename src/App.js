f0a18bfad47a8551e4cdfa6518a989f0b87172ca
JadenLund <lundjaden@gmail.com>
2023-05-27 11:16:54 -0500

    adding in app.js code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import ClickIncrease from "./components/ClickIncrease";
+import HoverIncrease from "./components/HoverIncrease";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Render both of these components to the UI */}
+      <ClickIncrease />
+      <HoverIncrease />
     </div>
   );
 }
-
-export default App;
