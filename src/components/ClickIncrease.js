c268fb0b7165556108b277500a513d3daf9d876d
JadenLund <lundjaden@gmail.com>
2023-05-27 11:12:04 -0500

    setting up ClickIncrease

---
+++ src/components/ClickIncrease.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+function ClickIncrease() {
+    const [fontSize, setFontSize] = useState(10)
+    return (
+        <div>
+            <button onClick={() => setFontSize((size) => size + 1)}>
+                Increase with Click
+            </button>
+            <p style={{ fontSize }}>Size of font in onClick function: {fontSize}</p>
+        </div>
+    )
+
+}
+export default ClickIncrease;
